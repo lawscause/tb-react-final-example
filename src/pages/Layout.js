daa28dca2bed60b6451f9f94e30c43c45f2b1b57
Mark Lawson <mark.lawson@cleveranchor.com>
2022-03-29 18:43:31 -0400

    modified Layout to include bootstrap css, using state to handle collapsable nav bar

---
+++ src/pages/Layout.js
@@ -0,0 +1,66 @@
+import { Outlet, NavLink } from "react-router-dom";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const Layout = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            Logo
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsibleNavbar"
+            aria-controls="collapsibleNavbar"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+            id="collapsibleNavbar"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" activeClassName="active">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/blogs"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Blogs
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/contact"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
